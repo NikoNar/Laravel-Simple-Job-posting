aabe2f1c540473c847f9987a5588d14ad6bd4864
NikoNar <nikoghosyannarek@gmail.com>
2022-07-23 12:00:33 +0400

    daily send coins to users logic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,16 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendCoins;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        SendCoins::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -16,6 +21,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('send:coin')->dailyAt('24:00');
     }
 
     /**
